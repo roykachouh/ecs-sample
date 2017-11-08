fcd2787e0498b422eff7af242e1b582ef66d85bb
Roy Kachouh <roykachouh@gmail.com>
2017-11-07 17:02:42 -0800

    Delete docker wiring

--- src/main/kotlin/com/kachouh/ecs/sample/model/Greeting.kt
+++ src/main/kotlin/com/kachouh/ecs/sample/model/Greeting.kt
@@ -1,5 +1,3 @@
 package com.kachouh.ecs.sample.model
 
-import com.github.dockerjava.api.model.Container
-
-data class Greeting(val name: String, val containers: List<Container>)
+data class Greeting(val name: String)
--- src/main/kotlin/com/kachouh/ecs/sample/web/HelloEcsController.kt
+++ src/main/kotlin/com/kachouh/ecs/sample/web/HelloEcsController.kt
@@ -4,14 +4,13 @@ import com.kachouh.ecs.sample.model.Greeting
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import java.util.*
 
 @RestController
 class HelloEcsController {
 
     @GetMapping("/hello-ecs")
     fun helloEcs(@RequestParam(value = "name", defaultValue = "Roy") name: String): Greeting {
-        return Greeting(name, Collections.emptyList())
+        return Greeting(name)
     }
 }
 
