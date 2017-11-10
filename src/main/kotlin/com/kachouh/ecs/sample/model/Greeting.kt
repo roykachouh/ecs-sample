ba98be33eebf75352883146b9a94d573f7b07dd7
Roy Kachouh <roykachouh@gmail.com>
2017-11-10 14:53:20 -0800

    Add list cluter results

--- src/main/kotlin/com/kachouh/ecs/sample/model/Greeting.kt
+++ src/main/kotlin/com/kachouh/ecs/sample/model/Greeting.kt
@@ -1,6 +1,9 @@
 package com.kachouh.ecs.sample.model
 
+import com.amazonaws.services.ecs.model.ListClustersResult
+
 data class Greeting(
         val name: String,
-        val hostname: String
+        val hostname: String,
+        val clusters: ListClustersResult
 )
--- src/main/kotlin/com/kachouh/ecs/sample/web/HelloEcsController.kt
+++ src/main/kotlin/com/kachouh/ecs/sample/web/HelloEcsController.kt
@@ -8,16 +8,15 @@ import org.springframework.web.bind.annotation.RestController
 import java.net.InetAddress
 
 @RestController
-class HelloEcsController (val fetchClusters: FetchClusters) {
+class HelloEcsController(val fetchClusters: FetchClusters) {
 
     @GetMapping("/hello-ecs")
     fun helloEcs(@RequestParam(value = "name", defaultValue = "Roy") name: String): Greeting {
-        val servicesResult = fetchClusters.fetch()
-        return Greeting(name, getHost())
+        return Greeting(name, getHost(), fetchClusters.fetch())
     }
 
     companion object {
-        fun getHost() : String = InetAddress.getLocalHost().hostAddress
+        fun getHost(): String = InetAddress.getLocalHost().hostAddress
     }
 }
 
