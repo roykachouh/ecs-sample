67af579356842983accaaea1d82a48501117b5d4
Roy Kachouh <roykachouh@gmail.com>
2017-11-20 14:49:47 -0800

    Remove dead test

--- src/main/kotlin/com/kachouh/ecs/sample/ecs/FetchClusters.kt
+++ src/main/kotlin/com/kachouh/ecs/sample/ecs/FetchClusters.kt
@@ -3,9 +3,7 @@ package com.kachouh.ecs.sample.ecs
 import com.amazonaws.services.ecs.AmazonECS
 import org.springframework.stereotype.Component
 
-
 @Component
 class FetchClusters(val ecs: AmazonECS) {
     fun fetch() = ecs.listClusters()
 }
-
