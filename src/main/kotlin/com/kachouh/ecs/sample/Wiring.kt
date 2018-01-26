60aadaa57c474fbd29cae73688a76c7be61bd99e
Roy Kachouh <roykachouh@gmail.com>
2018-01-26 13:21:50 -0800

    Add x-ray instrumentation

--- src/main/kotlin/com/kachouh/ecs/sample/Wiring.kt
+++ src/main/kotlin/com/kachouh/ecs/sample/Wiring.kt
@@ -3,7 +3,9 @@ package com.kachouh.ecs.sample
 import com.amazonaws.auth.AWSStaticCredentialsProvider
 import com.amazonaws.auth.BasicAWSCredentials
 import com.amazonaws.services.ecs.AmazonECSClientBuilder
+import com.amazonaws.xray.javax.servlet.AWSXRayServletFilter
 import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.support.beans
 import org.springframework.web.client.RestTemplate
 
@@ -19,5 +21,15 @@ fun beans() = beans {
         clientBuilder.region = "us-west-2"
 
         clientBuilder.build()
+    }
+
+    bean {
+        val registration = FilterRegistrationBean<AWSXRayServletFilter>()
+        registration.filter = AWSXRayServletFilter()
+        registration.addUrlPatterns("*")
+        registration.setName("XRayServletFilter")
+        registration.addInitParameter("fixedName", "roy-sample-service")
+        registration.order = 1
+        registration
     }
 }
