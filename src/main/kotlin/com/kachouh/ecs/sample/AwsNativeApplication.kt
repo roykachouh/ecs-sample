06a18ffe6532b7afa37009bc5970bcb401004afa
Roy Kachouh <roykachouh@gmail.com>
2018-03-02 11:58:13 -0800

    Add logger in kotlin

--- src/main/kotlin/com/kachouh/ecs/sample/AwsNativeApplication.kt
+++ src/main/kotlin/com/kachouh/ecs/sample/AwsNativeApplication.kt
@@ -1,11 +1,27 @@
 package com.kachouh.ecs.sample
 
+import org.slf4j.LoggerFactory
 import org.springframework.boot.SpringApplication.run
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import java.lang.Thread.sleep
+import java.util.*
+import kotlin.concurrent.thread
 
 @SpringBootApplication
 class AwsNativeApplication
 
+
+val LOG = LoggerFactory.getLogger(AwsNativeApplication::class.java.name)
+
 fun main(args: Array<String>) {
     run(AwsNativeApplication::class.java, *args)
+
+
+    thread(start = true) {
+        while(true) {
+            LOG.info("helloooooo: " + UUID.randomUUID().toString())
+            sleep((Random().nextInt(1000 - 500 + 1) + 500).toLong())
+        }
+    }
 }
+
